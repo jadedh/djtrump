2c1ba2d19f52d5c6ee1966a0c1c612cdfb3375f3
jadedh <jadedh@bu.edu>
2018-06-29 10:50:55 -0400

    ssh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-ssh root@104.236.57.112 <<EOF
+#root@104.236.57.112 <<EOF
+ssh -i ~/.ssh/JadeSshKey Jadedh@10.195.149.19 <<EOF
   cd djtrump
   git pull
   source /opt/envs/djtrump/bin/activate
